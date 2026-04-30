753175592179e0f56f7c6c54beaebad8c7c92737
Gustavo ALves <gustavo.a.moreira6@aluno.senai.br>
2026-04-30 13:24:48 -0300

    pequenas mudanças no styles

--- src/styles/stylessContainer.js
+++ src/styles/stylessContainer.js
@@ -6,27 +6,22 @@ export const stylessContainer = StyleSheet.create({
     container: {
         backgroundColor: "#000000ff",
         flex: 1
-
     },
 
     containerCima: {
         width: "100%",
         height: "100%",
         position: "relative",
-        borderTopRightRadius: 20,
-        borderTopLeftRadius: 20,
         zIndex: 8
     },
 
     containerBaixo: {
         backgroundColor: "#fff",
         width: "80%",
-        height: "80%",
+        height: "95%",
         position: 'absolute',
         borderTopRightRadius: 20,
-        borderTopLeftRadius: 20,
-        alignContent: center,
-        alignItems: center,
+        borderTopLeftRadius: 30,
         zIndex: 10
 
     }
